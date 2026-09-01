e123f34b60718056671749578b6b6b094aac55b3
dec256 <202341919+dec256@users.noreply.github.com>
2026-09-01 09:35:40 +0100

    clean up confmic

--- confmic/target/debug/confmic.d
+++
@@ -1,1 +0,0 @@
-/home/decwa/nix/confmic/target/debug/confmic: /home/decwa/nix/confmic/src/main.rs
--- confmic/target/debug/deps/confmic-f421c8ed418bccde.d
+++
@@ -1,5 +0,0 @@
-/home/decwa/nix/confmic/target/debug/deps/confmic-f421c8ed418bccde.d: src/main.rs
-
-/home/decwa/nix/confmic/target/debug/deps/confmic-f421c8ed418bccde: src/main.rs
-
-src/main.rs:
